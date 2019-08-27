88997484df9302eadad464fdbae3241822f47bf5
Noskcaj <kcajdev@gmail.com>
2019-08-26 23:53:41 -0400

    Add image url row

--- utils/init_db.py
+++ utils/init_db.py
@@ -12,6 +12,7 @@ cur.execute(
 create table if not exists articles (
     title        text not null,
     url          text not null,
+    image_url    text,
     site_name    text not null,
     site_url     text not null,
     author_name  int not null,

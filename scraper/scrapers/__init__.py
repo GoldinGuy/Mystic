ae71796980a805e5dc7ae2372a99631cff1c722f
Noskcaj <kcajdev@gmail.com>
2019-08-27 01:48:07 -0400

    Add Channel Fireball scraper

--- scraper/scrapers/__init__.py
+++ scraper/scrapers/__init__.py
@@ -2,6 +2,7 @@ from typing import Type, List
 
 from .types import ScraperBase
 from .mtggoldfish import MTGGoldfishScraper
+from .channelfireball import ChannelFireballScraper
 
 
-ALL_SCRAPERS: List[Type[ScraperBase]] = [MTGGoldfishScraper]
+ALL_SCRAPERS: List[Type[ScraperBase]] = [MTGGoldfishScraper, ChannelFireballScraper]
---
+++ scraper/scrapers/channelfireball.py
@@ -0,0 +1,38 @@
+from typing import List
+import json
+import requests
+
+from .types import ScraperBase, Article
+
+SITE_NAME = "ChannelFireball"
+BASE_URL = "https://store.channelfireball.com/landing"
+ARTICLES_TEMPLATE = (
+    "https://chfireball.wpengine.com/wp-json/wp/v2/multiple-post-type?"
+    "&type[]=post&type[]=video&per_page=15"
+)
+
+
+class ChannelFireballScraper(ScraperBase):
+    def scrape_articles(self) -> List[Article]:
+        # server rejects the default requests UA
+        content = requests.get(
+            ARTICLES_TEMPLATE.format(1), headers={"User-Agent": "Chrome/76.0.3809.110"}
+        ).json()
+
+        articles = []
+
+        for entry in content:
+
+            article = Article(
+                entry["title"],
+                entry["link"],
+                entry["featured_image"]["thumb"],
+                SITE_NAME,
+                BASE_URL,
+                entry["author"],
+                None,
+            )
+
+            articles.append(article)
+
+        return articles

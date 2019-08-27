11ced7015183a8000e48c58328a563955cc48b7c
Noskcaj <kcajdev@gmail.com>
2019-08-26 23:30:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mystic
+
+Mystic scraper prototype
+
+Install dependencies with:
+
+    pip3 install -r requirements.txt -U
+
---
+++ utils/init_db.py
@@ -0,0 +1,24 @@
+import os
+import psycopg2
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+conn = psycopg2.connect(DATABASE_URL)  # , sslmode='require')
+cur = conn.cursor()
+
+print("Creating articles table...")
+cur.execute(
+    """
+create table if not exists articles (
+    title        text not null,
+    url          text not null,
+    site_name    text not null,
+    site_url     text not null,
+    author_name  int not null,
+    author_url   text
+);"""
+)
+
+conn.commit()
+cur.close()
+conn.close()

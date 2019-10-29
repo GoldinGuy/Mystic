5e706fd1480c2e1c15e0c346dd4e7e6f2e716c25
Noskcaj <kcajdev@gmail.com>
2019-10-29 08:15:14 -0400

    Fix channel fireball, (switch to html scraping)

--- scraper/scrapers/channelfireball.py
+++ scraper/scrapers/channelfireball.py
@@ -1,14 +1,13 @@
 from typing import List
 from datetime import datetime
 import requests
-import html
+import lxml.html
 
 from .types import ScraperBase, Article
 
-BASE_URL = "https://store.channelfireball.com/landing"
+BASE_URL = "https://www.channelfireball.com/all-strategy"
 ARTICLES_TEMPLATE = (
-    "https://chfireball.wpengine.com/wp-json/wp/v2/multiple-post-type?"
-    "&type[]=post&per_page=15&page={}"
+    "https://www.channelfireball.com/all-strategy/category/articles/page/{}"
 )
 
 
@@ -20,21 +19,39 @@ class ChannelFireballScraper(ScraperBase):
         content = requests.get(
             ARTICLES_TEMPLATE.format(page),
             headers={"User-Agent": "Chrome/76.0.3809.110"},
-        ).json()
+        ).text
+        content = lxml.html.fromstring(content)
+
+        posts = content.xpath("//article/div/div")
 
         articles = []
 
-        for entry in content:
+        for post in posts:
+            header_node = post.xpath("header")[0]
+            title_node = header_node.xpath("h2/a")[0]
+            title = title_node.text
+            url = title_node.attrib["href"]
+
+            author_url = header_node.xpath("div/span/a")[0].attrib["href"]
+            author_name = header_node.xpath("div/span/a/span")[0].text
+
+            date_str = header_node.xpath("div/span[2]/span")[0].text.strip()
+            date = datetime.strptime(date_str, "%B %d, %Y")
+
+            img_url = post.xpath("div/div/a/img")[0].attrib["src"]
+
+            desc = post.xpath("div[2]/p")[0].text
+
             article = Article(
-                html.unescape(entry["title"]),
-                entry["link"],
-                datetime.fromisoformat(entry["date"]),
-                entry["featured_image"]["thumb"],
+                title,
+                url,
+                date,
+                img_url,
                 self.SITE_NAME,
                 BASE_URL,
-                entry["author"],
-                None,
-                html.unescape(entry["excerpt"]),
+                author_name,
+                author_url,
+                desc,
             )
 
             articles.append(article)
